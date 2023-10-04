cf264e747046538b08c262243c3565cf278259bc
Sourav Kumar <sk4118251@gmail.com>
2023-10-04 23:27:33 +0530

    updated the code

--- Dsa/code/linkedlist.cpp
+++ Dsa/code/linkedlist.cpp
@@ -10,12 +10,29 @@ public:
         this->data = data;
         this->next = NULL;
     }
-    
- 
+
 };
+void insertnode(node* &head,int d){
+    node* temp = new node(d);
+    temp->next = head;
+    head = temp;
+
+}
+void print(node* &head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+}
 int main()
 {
     node *node1 = new node(1);
-    cout << node1->data << endl;
-    cout << node1->next << endl;
+    node* head = node1;
+    print(head);
+    insertnode(head,20);
+    print(head);
+    
 }
